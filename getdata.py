58278d97a1f817366ab2a0b2ec782876de432256
marshyski <timski@linux.com>
2015-01-15 18:03:39 -0500

    Added getdata.py script

---
+++ getdata.py
@@ -0,0 +1,15 @@
+import requests, os, tarfile
+
+url = 'http://www.ipdeny.com/ipblocks/data/countries/all-zones.tar.gz'
+tar = os.path.basename(url)
+r = requests.get(url, stream=True)
+
+if r.status_code == 200:
+   f = open(tar, 'wb')
+   f.write(r.content)
+   f.close()
+
+if tarfile.is_tarfile(tar):
+    tarfile.open(tar).extractall('data')
+else:
+    print tar + " is not a tarfile"
